be74e3ff3ddcd25fc0f79988d9c47b913dd6ae66
carlosestrada22 <carlosg.estrada94@gmail.com>
2018-11-28 23:18:42 -0600

    feat: prod build

---
+++ build/precache-manifest.25cc76a16799adc6332f308ea6c0f97d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea36cd9a0e9eee97012a67b8a4570d7b",
+    "url": "/static/media/Roboto-Light.ea36cd9a.woff"
+  },
+  {
+    "revision": "da2511480b0da0a502e8",
+    "url": "/static/css/main.747bb1ba.chunk.css"
+  },
+  {
+    "revision": "eed9aab5449cc9c8430d7d258108f602",
+    "url": "/static/media/Roboto-Bold.eed9aab5.woff"
+  },
+  {
+    "revision": "d599ea5d579a6f335fab",
+    "url": "/static/js/1.d599ea5d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44b78f142603eb69f593ed4002ed7a4a",
+    "url": "/static/media/Roboto-Thin.44b78f14.woff"
+  },
+  {
+    "revision": "1f35e6a11d27d2e10d28946d42332dc5",
+    "url": "/static/media/Roboto-Thin.1f35e6a1.woff2"
+  },
+  {
+    "revision": "3c37aa69cd77e6a53a067170fa8fe2e9",
+    "url": "/static/media/Roboto-Light.3c37aa69.woff2"
+  },
+  {
+    "revision": "da2511480b0da0a502e8",
+    "url": "/static/js/main.da251148.chunk.js"
+  },
+  {
+    "revision": "cf4d60bc0b1d4b2314085919a00e1724",
+    "url": "/static/media/Roboto-Medium.cf4d60bc.woff"
+  },
+  {
+    "revision": "3cf6adf61054c328b1b0ddcd8f9ce24d",
+    "url": "/static/media/Roboto-Regular.3cf6adf6.woff"
+  },
+  {
+    "revision": "1561b424aaef2f704bbd89155b3ce514",
+    "url": "/static/media/Roboto-Medium.1561b424.woff2"
+  },
+  {
+    "revision": "5136cbe62a63604402f2fedb97f246f8",
+    "url": "/static/media/Roboto-Regular.5136cbe6.woff2"
+  },
+  {
+    "revision": "c0f1e4a4fdfb8048c72e86aadb2a247d",
+    "url": "/static/media/Roboto-Bold.c0f1e4a4.woff2"
+  },
+  {
+    "revision": "d599ea5d579a6f335fab",
+    "url": "/static/css/1.74ebb423.chunk.css"
+  },
+  {
+    "revision": "b92675fbd6c0b84d1e577c93d0da779d",
+    "url": "/index.html"
+  }
+];
