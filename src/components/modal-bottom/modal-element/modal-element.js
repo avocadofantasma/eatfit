a50ec69214f78d764ca37446a5e20e89a44d1bb5
carlosestrada22 <carlosg.estrada94@gmail.com>
2018-11-28 23:08:52 -0600

    feat: share buttons and warning removals

--- src/components/modal-bottom/modal-element/modal-element.js
+++ src/components/modal-bottom/modal-element/modal-element.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ModalElement = ({ title = "", icon = "grade", url = '#!', ytLink = "" }) => {
     return (
         <li className="collection-item avatar">
-            <a href={`"https://www.youtube.com/${ytLink}`} target="_blank">
+            <a href={`"https://www.youtube.com/${ytLink}`} target="_blank" rel="noopener noreferrer">
                 <i className="material-icons circle red">play_arrow</i>
                 <h1 className="title">{title}</h1>
                 <span href={url} className="secondary-content"><i className="material-icons">{icon}</i></span>
